e8004b16c92d30272065201726bed1ff4c2ebf10
Robert Leahy <rleahy@rleahy.ca>
2013-09-21 21:47:48 -0700

    MCPP Info Improvements
    /info mcpp now reports the minecraft compatibility of mcpp.dll.

--- src/info/mcpp.cpp
+++ src/info/mcpp.cpp
@@ -1,4 +1,8 @@
 #include <info/info.hpp>
+#include <packet.hpp>
+
+
+using namespace MCPP;
 
 
 static const String name("MCPP Information");
@@ -7,6 +11,8 @@ static const String identifier("mcpp");
 static const String help("Display information about mcpp.dll.");
 static const String mcpp_banner("MINECRAFT++:");
 static const String compiled_by_template("Compiled by {0} on {1}");
+static const String minecraft_compat("Compatible with Minecraft {0} (protocol version {1})");
+static const String version_template("{0}.{1}.{2}");
 
 
 class MCPPInfo : public Module, public InformationProvider {
@@ -60,6 +66,17 @@ class MCPPInfo : public Module, public InformationProvider {
 							compiled_by_template,
 							Server::Get().CompiledWith(),
 							Server::Get().BuildDate()
+						)
+					<<	Newline
+					<<	String::Format(
+							minecraft_compat,
+							String::Format(
+								version_template,
+								MinecraftMajorVersion,
+								MinecraftMinorVersion,
+								MinecraftSubminorVersion
+							),
+							ProtocolVersion
 						);
 		
 		}
