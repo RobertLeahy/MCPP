428665412c18146d2306593e9edfbd38b4c9e293
Robert Leahy <rleahy@rleahy.ca>
2013-09-27 02:31:39 -0700

    Makefile Fix -- Players
    Fixed an error in player.mk which was causing mcpp_player.dll to be unreliably and incorrectly rebuilt.

--- player.mk
+++ player.mk
@@ -3,13 +3,13 @@ mods: bin/mods/mcpp_player.dll
 
 bin/mods/mcpp_player.dll: \
 $(MOD_OBJ) \
-src/player/event_handlers.cpp \
-src/player/get_player.cpp \
-src/player/player.cpp \
-src/player/players.cpp \
-src/player/player_position.cpp \
-src/player/set_spawn.cpp \
-src/player/update_position.cpp | \
+obj/player/event_handlers.o \
+obj/player/get_player.o \
+obj/player/player.o \
+obj/player/players.o \
+obj/player/player_position.o \
+obj/player/set_spawn.o \
+obj/player/update_position.o | \
 $(MOD_LIB) \
 bin/mods/mcpp_entity_id.dll \
 bin/mods/mcpp_world.dll
