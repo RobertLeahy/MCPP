c966a3b104d6e5981ef246507a0638bd9e1c4daf
Robert Leahy <rleahy@rleahy.ca>
2013-11-13 16:29:40 -0800

    Information Update
    Updated the information provider to conform to the new Command interface.
    
    Also rewrote the code and made several improvements:
    
    There's no longer a dependency on common.hpp.
    
    A hash map is now used to lookup information providers, which should lead to a lookup speed improvement when many information providers are installed.
    
    The /help info now capitalizes on the ability to use formatting codes to make the help clearer/more readable.
    
    The code is now clearer/more readable.
    
    The code uses the availability of a status return value to communicate failure reasons to the command-handling code, leveraging consistent/automated error reporting (rather than rolling its own).

--- src/info/main.cpp
+++ src/info/main.cpp
@@ -1,8 +1,6 @@
 #include <info/info.hpp>
-#include <op/op.hpp>
+#include <permissions/permissions.hpp>
 #include <singleton.hpp>
-#include <utility>
-#include <algorithm>
 
 
 using namespace MCPP;
@@ -13,22 +11,48 @@ namespace MCPP {
 
 	static const String name("Information Provider");
 	static const String identifier("info");
+	//	The permission which, when granted, allows
+	//	users to invoke /info
+	static const String permission("info");
 	static const String summary("Provides information about various server internals.");
 	static const Word priority=1;
-	static const String info_banner="====INFORMATION====";
-	static const String log_prepend("Information Provider: ");
-	static const String mods_dir("info_mods");
+	static const String info_banner("====INFORMATION====");
+	static const String help("Syntax: ");
+	static const String syntax("<provider>");
+	static const String providers_banner("Providers:");
+	static const String separator(" - ");
+	static const String arg_separator(" ");
 	
 	
-	static String help;
+	InformationProvider * Information::get (const String & identifier) {
+	
+		auto iter=map.find(identifier);
+		
+		return (iter==map.end()) ? nullptr : iter->second;
+	
+	}
 	
 	
-	static const Regex split("\\s+");
+	static Singleton<Information> singleton;
+	
+	
+	Information & Information::Get () noexcept {
+	
+		return singleton.Get();
+	
+	}
 	
 	
 	Word Information::Priority () const noexcept {
 	
 		return priority;
+	
+	}
+	
+	
+	void Information::Install () {
+	
+		Commands::Get().Add(identifier,this);
 	
 	}
 	
@@ -40,169 +64,191 @@ namespace MCPP {
 	}
 	
 	
-	const String & Information::Identifier () const noexcept {
+	void Information::Summary (const String &, ChatMessage & message) {
 	
-		return identifier;
-	
-	}
-	
-	
-	const String & Information::Help () const noexcept {
-	
-		return help;
+		message << summary;
 	
 	}
 	
 	
-	const String & Information::Summary () const noexcept {
+	void Information::Help (const String &, ChatMessage & message) {
 	
-		return summary;
-	
-	}
-	
-	
-	bool Information::Check (SmartPointer<Client> client) const {
-	
-		return Ops::Get().IsOp(client->GetUsername());
-	
-	}
-	
-	
-	Vector<InformationProvider *> Information::retrieve (const Regex & regex) {
-	
-		Vector<InformationProvider *> retr;
-		
-		for (auto * ip : providers)
-		if (regex.IsMatch(ip->Identifier()))
-		retr.Add(ip);
-		
-		return retr;
+		message	<<	help
+				<<	ChatStyle::Bold
+				<<	"/"
+				<<	identifier
+				<<	arg_separator
+				<<	syntax
+				<<	Newline
+				<<	providers_banner
+				<<	ChatFormat::Pop;
+				
+		//	Loop over providers and print name
+		//	and summary for each
+		for (auto ptr : list) message	<<	Newline
+										<<	ChatStyle::Bold
+										<<	ptr->Identifier()
+										<<	ChatFormat::Pop
+										<<	separator
+										<<	ptr->Help();
 	
 	}
 	
 	
-	InformationProvider * Information::retrieve (const String & name) {
-	
-		for (auto * ip : providers)
-		if (name==ip->Identifier())
-		return ip;
-		
-		return nullptr;
-	
-	}
-	
-	
-	Vector<String> Information::AutoComplete (const String & args_str) const {
+	Vector<String> Information::AutoComplete (const CommandEvent & event) {
 	
 		Vector<String> retr;
 		
-		auto args=split.Split(args_str);
+		//	How we proceed depends on the number of
+		//	arguments given.
+		//
+		//	If zero arguments are given, our suggestion
+		//	is any of the providers that we have installed
+		//
+		//	If one argument is given, our suggestion is any
+		//	of the providers whose identifiers start with that
+		//	string.
+		//
+		//	If more than one argument is given, we can't
+		//	suggest anything, since this command takes only
+		//	one argument
+		switch (event.Arguments.Count()) {
 		
-		if (args.Count()<=1)
-		for (auto * ip : (
-			(args.Count()==0)
-				?	providers
-				:	const_cast<Information *>(this)->retrieve(
-						Regex(
-							String::Format(
-								"^{0}",
-								Regex::Escape(
-									args[0]
-								)
-							)
+			case 0:
+				for (auto ptr : list) retr.Add(ptr->Identifier());
+				break;
+			case 1:{
+			
+				//	Create an appropriate regular
+				//	expression
+				Regex regex(
+					String::Format(
+						"^{0}",
+						Regex::Escape(
+							event.Arguments[0]
 						)
 					)
-		)) retr.Add(ip->Identifier());
+				);
+				
+				//	Loop and find matches
+				for (auto ptr : list) if (regex.IsMatch(ptr->Identifier())) retr.Add(ptr->Identifier());
+			
+			}break;
+			default:break;
+		
+		}
 		
 		return retr;
 	
 	}
 	
 	
-	bool Information::Execute (SmartPointer<Client>, const String & args_str, ChatMessage & message) {
+	bool Information::Check (const CommandEvent & event) {
+	
+		//	Null client pointer means that this
+		//	is issued by the console, which has
+		//	all permissions
+		if (event.Issuer.IsNull()) return true;
 		
-		auto args=split.Split(args_str);
-		
-		//	If there's no argument, or more
-		//	than one, that's a syntax error
-		if (args.Count()!=1) return false;
-		
-		//	Attempt to retrieve the appropriate
-		//	information provider.  If there
-		//	isn't one, that's a syntax error
-		auto * ip=retrieve(args[0]);
-		if (ip==nullptr) return false;
-		
-		//	Prepare a chat message
-		message	<< ChatStyle::Bold
-				<< ChatStyle::Yellow
-				<< info_banner
-				<< ChatFormat::Pop
-				<< ChatFormat::Pop
-				<< Newline;
-				
-		//	Get rest of chat message from
-		//	provider
-		ip->Execute(message);
-		
-		return true;
+		//	Check to see if this user is granted
+		//	the appropriate permission
+		return Permissions::Get().GetUser(event.Issuer).Check(permission);
 	
 	}
 	
 	
-	void Information::Install () {
+	CommandResult Information::Execute (CommandEvent event) {
+	
+		CommandResult retr;
 		
-		//	Install ourselves into the command
-		//	module
-		Commands::Get().Add(this);
+		//	Exactly one argument must be provided
+		if (event.Arguments.Count()!=1) {
+		
+			retr.Status=CommandStatus::SyntaxError;
+			
+			return retr;
+		
+		}
+		
+		//	Attempt to get the associated information provider
+		auto ptr=get(event.Arguments[0]);
+		
+		if (ptr==nullptr) {
+		
+			retr.Status=CommandStatus::DoesNotExist;
+			
+			return retr;
+		
+		}
+		
+		//	Execute
+		
+		retr.Message	<<	ChatStyle::Yellow
+						<<	ChatStyle::Bold
+						<<	info_banner
+						<<	ChatFormat::Pop
+						<<	ChatFormat::Pop
+						<<	Newline;
+						
+		ptr->Execute(retr.Message);
+		
+		retr.Status=CommandStatus::Success;
+		
+		return retr;
 	
 	}
 	
 	
 	void Information::Add (InformationProvider * provider) {
 	
-		if (provider!=nullptr) {
+		//	If a null pointer was passed in,
+		//	do nothing
+		if (provider==nullptr) return;
 		
-			//	Insert sorted
+		//	Attempt to insert into map
 		
-			//	Find insertion point
-			Word i=0;
+		auto iter=map.find(provider->Identifier());
+		
+		//	If a provider by this name exists
+		//	in the map, replace it, otherwise
+		//	create a new entry in the map
+		if (iter==map.end()) {
+		
+			auto pair=map.emplace(
+				provider->Identifier(),
+				provider
+			);
+			
+			iter=pair.first;
+		
+		} else {
+		
+			iter->second=provider;
+		
+		}
+		
+		//	Attempt to insert into list in sorted
+		//	order.  If that fails, roll back and
+		//	remove from map
+		try {
+		
+			Word loc=0;
 			for (
 				;
-				(i<providers.Count()) &&
-				(providers[i]->Identifier()<provider->Identifier());
-				++i
+				(loc<list.Count()) &&
+				(list[loc]->Identifier()<provider->Identifier());
+				++loc
 			);
 			
-			//	Insert
-			providers.Insert(
-				provider,
-				i
-			);
-			
-			//	Rebuild help
-			//
-			//	CONSIDER OPTIMIZING
-			help=String();
-			for (const auto * ip : providers) {
-			
-				if (help.Size()!=0) help << Newline;
-				
-				help << "/info " << ip->Identifier() << " - " << ip->Help();
-			
-			}
-			
+			list.Insert(provider,loc);
+		
+		} catch (...) {
+		
+			map.erase(iter);
+		
+			throw;
+		
 		}
-	
-	}
-	
-	
-	static Singleton<Information> singleton;
-	
-	
-	Information & Information::Get () noexcept {
-	
-		return singleton.Get();
 	
 	}
 
